ae3442b773798b691eecc3e2aa77024caf400366
Aaron Parker <aaron.parker@b2bfinance.com>
2023-06-21 14:34:31 +0100

    feat: fix image editing

---
+++ assets/scopeId-625451a5.js
@@ -0,0 +1,1 @@
+import{an as n}from"./index-69c86b32.js";function c(){const e=n("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
---
+++ assets/useGrazeAPI-dc3df87f.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-69c86b32.js";const t=e.getInstance(),s=()=>t;export{s as u};
