1215a7d42e1e134e838d654115b7922de26bcb86
Aaron Parker <github@forester.knightmare.org.uk>
2023-08-17 12:28:15 +0100

    feat: add pdf, improve description

--- assets/useAvatar-f8aaa83a.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./useStoreAPI-810ca54c.js";const r=t(),s=()=>e=>`${r.GetProfileImageURL(e)}?${new Date().getTime()}`;export{s as u};
---
+++ assets/useStoreAPI-09fb5996.js
@@ -0,0 +1,1 @@
+import{az as t}from"./index-5f076c3f.js";const e=t.getInstance(),r=()=>e;export{r as u};
