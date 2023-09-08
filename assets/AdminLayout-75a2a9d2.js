0b69e7e1c79d220666e9a8fe4998f85228d21109
Aaron Parker <github@forester.knightmare.org.uk>
2023-09-08 02:32:44 +0100

    feat: express test

--- assets/AdminLayout-75a2a9d2.js
+++
@@ -1,1 +0,0 @@
-import{I as t,a5 as r,L as n,M as s,N as a}from"./index-79008dac.js";import{p}from"./performLoginCheck-7624f362.js";import"./userData-f4c7febb.js";const l=t({__name:"AdminLayout",setup(c){const o=r();return p(o),(m,u)=>{const e=n("RouterView");return s(),a(e)}}});export{l as default};
--- assets/performLoginCheck-7624f362.js
+++
@@ -1,1 +0,0 @@
-import{$ as s}from"./index-79008dac.js";import{G as n,a as o}from"./userData-f4c7febb.js";const m=e=>{const t=n();s.getInstance().getTokenByService("GrazeAPI")?t||o().then(()=>{}).catch(()=>{e.push("/").then(()=>{})}):e.push("/")};export{m as p};
