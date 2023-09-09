99faa8e4e0520b19996d305b9ef793fdfdb939c6
Aaron Parker <github@forester.knightmare.org.uk>
2023-09-09 12:48:32 +0100

    fix: standardise labels

---
+++ assets/performLoginCheck-d92ab96f.js
@@ -0,0 +1,1 @@
+import{$ as s}from"./index-6e392453.js";import{G as n,a as o}from"./userData-c45cca30.js";const m=e=>{const t=n();s.getInstance().getTokenByService("GrazeAPI")?t||o().then(()=>{}).catch(()=>{e.push("/").then(()=>{})}):e.push("/")};export{m as p};
