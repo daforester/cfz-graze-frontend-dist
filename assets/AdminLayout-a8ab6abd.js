10edb66722bdaa4e2d2cb359f925d47cff3023f3
Aaron Parker <github@forester.knightmare.org.uk>
2023-09-12 15:47:26 +0100

    fix: update reg display on change layout

--- assets/AdminLayout-a8ab6abd.js
+++
@@ -1,1 +0,0 @@
-import{I as t,a5 as r,L as n,M as s,N as a}from"./index-93557928.js";import{p}from"./performLoginCheck-8f7eb54f.js";import"./userData-03172a77.js";const l=t({__name:"AdminLayout",setup(c){const o=r();return p(o),(m,u)=>{const e=n("RouterView");return s(),a(e)}}});export{l as default};
