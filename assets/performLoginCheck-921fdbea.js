99d77d371fa3bd7b44b6aeca46d60a00c21fd7f4
Aaron Parker <github@forester.knightmare.org.uk>
2023-10-27 02:39:35 +0100

    fix: tags inside card action

---
+++ assets/performLoginCheck-921fdbea.js
@@ -0,0 +1,1 @@
+import{$ as s}from"./index-f3974254.js";import{G as n,a as o}from"./userData-b9203568.js";const m=e=>{const t=n();s.getInstance().getTokenByService("GrazeAPI")?t||o().then(()=>{}).catch(()=>{e.push("/").then(()=>{})}):e.push("/")};export{m as p};
--- assets/scopeId-3e4db684.js
+++
@@ -1,1 +0,0 @@
-import{aX as s,b as c}from"./index-82da25d6.js";import{c as n}from"./createSimpleFunctional-deb04a41.js";const o=s.getInstance(),p=()=>o,u=n("v-spacer","div","VSpacer");function d(){const e=c("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{u as V,p as a,d as u};
