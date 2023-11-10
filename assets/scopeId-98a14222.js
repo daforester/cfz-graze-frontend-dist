8c4b2a9ab259ee9d6b6741ec72acee04122ef6ba
Aaron Parker <github@forester.knightmare.org.uk>
2023-11-10 15:24:59 +0000

    fix: remove debug

--- assets/scopeId-98a14222.js
+++
@@ -1,1 +0,0 @@
-import{aX as s,b as c}from"./index-4ff65b9c.js";import{c as n}from"./createSimpleFunctional-6503e3bb.js";const o=s.getInstance(),p=()=>o,u=n("v-spacer","div","VSpacer");function d(){const e=c("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{u as V,p as a,d as u};
---
+++ assets/useAvatar-eddd7995.js
@@ -0,0 +1,1 @@
+import{a as t}from"./scopeId-c91b2c6b.js";const r=t(),a=()=>e=>`${r.GetProfileImageURL(e)}?${new Date().getTime()}`;export{a as u};
