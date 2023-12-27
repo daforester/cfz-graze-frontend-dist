4ab5f63d6ca6d1c1045879389c4867aab10e9190
Aaron Parker <github@forester.knightmare.org.uk>
2023-12-27 23:10:13 +0000

    feat: increase scale speed

---
+++ assets/AdminLayout-5dacc624.js
@@ -0,0 +1,1 @@
+import{I as t,a6 as r,L as n,M as s,N as a}from"./index-e23e2f2e.js";import{p}from"./performLoginCheck-f4d06112.js";import"./userData-aca22c66.js";const l=t({__name:"AdminLayout",setup(c){const o=r();return p(o),(m,u)=>{const e=n("RouterView");return s(),a(e)}}});export{l as default};
---
+++ assets/useAvatar-58442662.js
@@ -0,0 +1,1 @@
+import{a as t}from"./scopeId-463895e4.js";const r=t(),a=()=>e=>`${r.GetProfileImageURL(e)}?${new Date().getTime()}`;export{a as u};
