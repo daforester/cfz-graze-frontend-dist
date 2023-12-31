a997831c32845925b2eacc093ec85f983e185b79
Aaron Parker <github@forester.knightmare.org.uk>
2023-12-31 03:08:40 +0000

    feat: new tags for extras

---
+++ assets/performLoginCheck-8633ce51.js
@@ -0,0 +1,1 @@
+import{$ as s}from"./index-7b1eb77b.js";import{G as n,a as o}from"./userData-2f1dcac3.js";const m=e=>{const t=n();s.getInstance().getTokenByService("GrazeAPI")?t||o().then(()=>{}).catch(()=>{e.push("/").then(()=>{})}):e.push("/")};export{m as p};
---
+++ assets/setupLogin-27816262.js
@@ -0,0 +1,1 @@
+import{ay as s,U as a}from"./index-7b1eb77b.js";import{S as r}from"./userData-2f1dcac3.js";const f=(o,t,i,n=!1)=>{s(o),r(t),a.getInstance().emit("login",t);let e="/";n&&(e="/profile"),i.push(e).then(()=>{}).catch(()=>{})};export{f as S};
