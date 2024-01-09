f7575d559db92d9882426ba290463ac28c0bc6e6
Aaron Parker <github@forester.knightmare.org.uk>
2024-01-09 14:42:55 +0000

    fix: additional debounce on search

---
+++ assets/LoginView-af96a363.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./LoginView.vue_vue_type_script_setup_true_lang-7944b76f.js";import"./index-5e9610d5.js";import"./emailFormVariant-e5e0aad4.js";import"./LoginButton-e0bddcb8.js";import"./index-26b71cef.js";import"./VBtn-85ca600c.js";import"./VContainer-a121345e.js";import"./VRow-2dd6ff78.js";import"./userData-708fca36.js";export{o as default};
---
+++ assets/performLoginCheck-62968d07.js
@@ -0,0 +1,1 @@
+import{$ as s}from"./index-5e9610d5.js";import{G as n,a as o}from"./userData-708fca36.js";const m=e=>{const t=n();s.getInstance().getTokenByService("GrazeAPI")?t||o().then(()=>{}).catch(()=>{e.push("/").then(()=>{})}):e.push("/")};export{m as p};
