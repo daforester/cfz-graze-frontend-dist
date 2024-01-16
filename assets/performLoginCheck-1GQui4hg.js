5864f57e5ea8e4f3f69b8fdc01a6efe240970119
Aaron Parker <github@forester.knightmare.org.uk>
2024-01-16 16:59:47 +0000

    feat: disable swipe on reg actions tabs

--- assets/performLoginCheck-1GQui4hg.js
+++
@@ -1,1 +0,0 @@
-import{T as s}from"./index-AeufC2vi.js";import{G as n,a as o}from"./userData-InK1f7HB.js";const m=e=>{const t=n();s.getInstance().getTokenByService("GrazeAPI")?t||o().then(()=>{}).catch(()=>{e.push("/").then(()=>{})}):e.push("/")};export{m as p};
--- assets/setupLogin-Z1rtthAU.js
+++
@@ -1,1 +0,0 @@
-import{aG as s,E as a}from"./index-AeufC2vi.js";import{S as r}from"./userData-InK1f7HB.js";const f=(o,t,i,n=!1)=>{s(o),r(t),a.getInstance().emit("login",t);let e="/";n&&(e="/profile"),i.push(e).then(()=>{}).catch(()=>{})};export{f as S};
