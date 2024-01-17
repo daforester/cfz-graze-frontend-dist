790da5353b08bcd97a7cb26593fb2912867fc59c
Aaron Parker <github@forester.knightmare.org.uk>
2024-01-17 17:49:49 +0000

    feat: demo nfc

--- assets/performLoginCheck-658f0b34.js
+++
@@ -1,1 +0,0 @@
-import{$ as s}from"./index-0e5dd427.js";import{G as n,a as o}from"./userData-7a4f5316.js";const m=e=>{const t=n();s.getInstance().getTokenByService("GrazeAPI")?t||o().then(()=>{}).catch(()=>{e.push("/").then(()=>{})}):e.push("/")};export{m as p};
---
+++ assets/useAvatar-5A4p6i_I.js
@@ -0,0 +1,1 @@
+import{a as t}from"./scopeId-b7IrjTfM.js";const r=t(),a=()=>e=>`${r.GetProfileImageURL(e)}?${new Date().getTime()}`;export{a as u};
