6075883a9bc468cfad1816efb2fc59a87932d97b
Aaron Parker <github@forester.knightmare.org.uk>
2024-02-10 14:31:40 +0000

    feat: new bg

--- assets/canViewOperations-pTrwOete.js
+++
@@ -1,1 +0,0 @@
-import{G as s}from"./group-7fhbY6WJ.js";import{C as e}from"./canDo-AeLGJhzt.js";const d=(i,r=void 0)=>{let o;return i===void 0?!1:(r===void 0?o=e(i,s.GROUP_TYPE_CONVENTION):o=e(i,s.GROUP_TYPE_CONVENTION,r.ConventionUUID,r),o.ConfigureLostAndFound||o.ConfigureProps||o.EditLostAndFound||o.EditProps||o.ViewLostAndFound||o.ViewProps)};export{d as C};
--- assets/useAvatar-C1OtERZk.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./scopeId-baCNzk1D.js";const r=t(),a=()=>e=>`${r.GetProfileImageURL(e)}?${new Date().getTime()}`;export{a as u};
