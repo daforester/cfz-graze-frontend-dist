8475bd50cf0db8b133ecb9b95b546d30aaf2057f
Aaron Parker <github@forester.knightmare.org.uk>
2024-07-14 01:49:21 +0100

    fix: accommodation list display after joining

--- assets/LoginView-FCuLfksp.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./LoginView.vue_vue_type_script_setup_true_lang-kRQpNvNk.js";import"./index-pTClsoYE.js";import"./emailFormVariant-1EY2p0cn.js";import"./LoginButton-Kq039TEM.js";import"./index-2XRRB2DS.js";import"./VContainer-TOR5bECh.js";import"./VRow-Umfr5mWv.js";import"./userData-kh7_geBS.js";export{o as default};
